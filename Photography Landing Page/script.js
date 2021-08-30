ff3fd7f111085f38414f228c77b863b86a081d6c
iamyourdeveloper <52364314+iamyourdeveloper@users.noreply.github.com>
2021-08-29 21:11:33 -0400

    Add files via upload

---
+++ Photography Landing Page/script.js
@@ -0,0 +1,46 @@
+// Animation Sequence for Images:
+setInterval(function(){
+    var container = document.getElementById('div-1b');
+    var tmp = container.innerHTML;
+    container.innerHTML= tmp;
+    }, 62500 // length of the whole show in milliseconds
+    )
+
+
+    const email = document.getElementById('email');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        checkInputs();
+    });
+
+    function checkInputs() {
+        const emailValue = email.value.trim();
+
+        if(emailValue === '') {
+            setErrorFor(email, 'Email cannot be blank');
+        } else if(!isEmail(emailValue)) {
+            setErrorFor(email, 'Email is not valid');
+        } else {
+            setSuccessFor(email);
+        }
+    }
+
+    function setErrorFor(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector('small');
+
+        small.innerText = message;
+
+        formControl.className = 'form-control error'
+    }
+
+    function setSuccessFor(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+    }
+
+    function isEmail(email) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    }
